e44effaf08b45518f36359ce7558d3f18bd6e535
LoggingNewMemory <albert.wesley.dion@gmail.com>
2025-10-03 20:34:09 +0700

    Change SUKISU Manual Huk to 1.5

--- build.sh
+++ build.sh
@@ -73,8 +73,6 @@ fi
 
 cd $KSRC
 
-## Xiaomi Garnet Patch from Goten
-
 ## KernelSU setup
 if ksu_included; then
   # Remove existing KernelSU drivers
@@ -92,7 +90,7 @@ if ksu_included; then
 
   # Install kernelsu
   case "$KSU" in
-  "Next") install_ksu pershoot/KernelSU-Next $(if susfs_included; then echo "next-susfs"; else echo "next"; fi) ;;
+  "Next") install_ksu KernelSU-Next/KernelSU-Next $(if susfs_included; then echo "next-susfs"; else echo "next"; fi) ;;
   "Suki") install_ksu SukiSU-Ultra/SukiSU-Ultra $(if susfs_included; then echo "susfs-main"; elif ksu_manual_hook; then echo "nongki"; else echo "main"; fi) ;;
   esac
   config --enable CONFIG_KSU
